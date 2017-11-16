aab038406c5b1f08d831da46d64c2074b8e8770c
dougajmcdonald <dougajmcdonald@gmail.com>
2017-11-16 20:33:32 +0000

    Remove dist

--- dist/parser/Interface/IMessage.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var MessageType;
-(function (MessageType) {
-    MessageType[MessageType["Error"] = 0] = "Error";
-    MessageType[MessageType["Warning"] = 1] = "Warning";
-    MessageType[MessageType["Info"] = 2] = "Info";
-})(MessageType = exports.MessageType || (exports.MessageType = {}));
