608191766f5c86a38e7673f3c6c464181a19db60
dougajmcdonald <dougajmcdonald@gmail.com>
2017-11-19 21:23:30 +0000

    add simple checks to stop breaking things in the wrong order

--- src/containers/simulator/index.js
+++ src/containers/simulator/index.js
@@ -11,8 +11,8 @@ import Core from './core';
 const Simulator = props => (
   <div>
     <h1>Core simulator</h1>
-    <button onClick={() => props.init(props.standardId, props.parseResult)}>Initialise Simulator</button>
-    <button onClick={() => props.step()}>Step</button>
+    {props.redcode && <button onClick={() => props.init(props.standardId, props.parseResult)}>Initialise Simulator</button>}
+    {props.isInitialised && <button onClick={() => props.step()}>Step</button>}
     <div>
       <textarea value={props.redcode} readOnly="readOnly" />
       <Core core={props.core} />
@@ -24,7 +24,8 @@ const mapStateToProps = state => ({
   redcode: state.parser.redcode,
   parseResult: state.parser.parseResult,
   standardId: state.parser.standardId,
-  core: state.simulator.core
+  core: state.simulator.core,
+  isInitialised: state.simulator.isInitialised
 })
 
 const mapDispatchToProps = dispatch => bindActionCreators({
