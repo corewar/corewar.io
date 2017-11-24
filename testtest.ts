df7b6406f3f0f8e7e7639c2573d1c6e4b1cda472
gareththegeek <gj_williams2000@yahoo.co.uk>
2017-11-24 19:57:52 +0000

    Add sinon to testtest, seems legit

--- testtest.ts
+++ testtest.ts
@@ -1,8 +1,16 @@
 import { expect } from "chai"
+import * as sinon from "sinon"
 
 describe("TestTest", () => {
 
     it("should test that testing works", () => {
+
+        var o = {
+            f: () => { return 1; }
+        };
+        sinon.stub(o, "f").returns(2);
+
+        expect(o.f()).to.be.equal(2);
 
         expect(1).to.be.equal(1);
     });
