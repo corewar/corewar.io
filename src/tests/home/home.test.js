d648c194608fe04f1d35d352a92da9e9e15a34f6
dougajmcdonald <dougajmcdonald@gmail.com>
2017-12-10 14:01:56 +0000

    remove tests for old components

--- src/tests/home/home.test.js
+++
@@ -1,8 +0,0 @@
-
-import React from 'react';
-import { shallow } from 'enzyme';
-import Home from '../../containers/home/index';
-
-it('renders without crashing', () => {
-  shallow(<Home.WrappedComponent />);
-});
