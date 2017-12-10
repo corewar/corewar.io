a28bff48e1c182952ddab0c74e265276e604785d
dougajmcdonald <dougajmcdonald@gmail.com>
2017-12-10 13:58:32 +0000

    remove redundant bits

--- src/containers/app/index.js
+++ src/containers/app/index.js
@@ -1,6 +1,5 @@
-import logo from '../../logo.png'
 import React from 'react'
-import { Route, NavLink } from 'react-router-dom'
+import { Route } from 'react-router-dom'
 
 import SiteHeader from './siteHeader'
 import Sidebar from './sidebar'
@@ -13,11 +12,6 @@ const App = () => (
   <div className="grid-container">
     <SiteHeader isAuthenticated={false}/>
     <Sidebar />
-      {/* <nav>
-        <NavLink to="/" exact={true} activeClassName="home-active" className="home">Home</NavLink>
-        <NavLink to="/parser" activeClassName="parser-active" className="parser">Parser</NavLink>
-        <NavLink to="/simulator" activeClassName="simulator-active" className="simulator">Simulator</NavLink>
-      </nav> */}
     <main>
       <Route exact path="/" component={DebugInterface} />
       <Route exact path="/parser" component={DebugInterface} />
