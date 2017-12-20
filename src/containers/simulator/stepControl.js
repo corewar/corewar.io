aaf58c35906a79364333ed872bbb89f382630df5
dougajmcdonald <dougajmcdonald@gmail.com>
2017-12-20 20:42:49 +0000

    add step control tests

--- src/containers/simulator/stepControl.js
+++ src/containers/simulator/stepControl.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import FontAwesome from 'react-fontawesome'
 
-const StepControl = ({ isInitialised, handleClick }) => (
-  <div className="simulatorControl"  onClick={isInitialised ? handleClick : null}>
+const StepControl = ({ isInitialised, isRunning, handleClick }) => (
+  <div className="simulatorControl"  onClick={isInitialised && !isRunning ? handleClick : null}>
     <span className="tooltip">step</span>
     <FontAwesome name="step-forward" size="2x"/>
   </div>
---
+++ src/tests/simulator/stepControl.test.js
@@ -0,0 +1,58 @@
+
+import React from 'react'
+import { shallow, mount } from 'enzyme'
+import { expect } from 'chai'
+import sinon from 'sinon'
+
+import StepControl from './../../containers/simulator/stepControl'
+import FontAwesome from 'react-fontawesome'
+
+it('renders without crashing', () => {
+  shallow(<StepControl />)
+});
+
+it('calls the clickHandler when the play button is clicked', () => {
+
+  const clickHandler = sinon.spy()
+
+  const props = {
+    isRunning: false,
+    handleClick: clickHandler
+  }
+
+  const wrapper = shallow(<StepControl {...props}/>)
+  wrapper.find(FontAwesome).simulate('click')
+
+  expect(clickHandler.calledOnce)
+});
+
+it('doesnt call the clickHandler when not initialised', () => {
+
+  const clickHandler = sinon.spy()
+
+  const props = {
+    isInitialised: false,
+    clickHandler: clickHandler
+  }
+
+  const wrapper = shallow(<StepControl {...props}/>)
+  wrapper.find(FontAwesome).simulate('click')
+
+  expect(clickHandler.called).to.equal(false)
+});
+
+it('doesnt call the clickHandler when running', () => {
+
+    const clickHandler = sinon.spy()
+
+    const props = {
+      isInitialised: true,
+      isRunning: false,
+      clickHandler: clickHandler
+    }
+
+    const wrapper = shallow(<StepControl {...props}/>)
+    wrapper.find(FontAwesome).simulate('click')
+
+    expect(clickHandler.called).to.equal(false)
+  });
