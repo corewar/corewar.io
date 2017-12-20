e6ae0914302b4560d6d25c92c2a804b3440ca9f0
dougajmcdonald <dougajmcdonald@gmail.com>
2017-12-20 17:16:27 +0000

    DESTROY colon

--- src/containers/simulator/playPauseControl.js
+++ src/containers/simulator/playPauseControl.js
@@ -3,7 +3,7 @@ import FontAwesome from 'react-fontawesome'
 
 const PlayPauseControl = ({ isRunning, handlePlay, handlePause }) => (
   <div className="simulatorControl"  onClick={isRunning ? handlePause : handlePlay}>
-    <FontAwesome name={isRunning ? `pause` : `play`} size="2x" /> :
+    <FontAwesome name={isRunning ? `pause` : `play`} size="2x" />
   </div>
 )
 
