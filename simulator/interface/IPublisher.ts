19d3c53e7a9f4488826330858b4499e84e793f49
gareththegeek <gj_williams2000@yahoo.co.uk>
2017-12-20 21:15:34 +0000

    Allow specific message types to be disabled

--- simulator/interface/IPublisher.ts
+++ simulator/interface/IPublisher.ts
@@ -1,8 +1,10 @@
-import { IMessage } from "./IMessage";
+import { IMessage, MessageType } from "./IMessage";
 import { IPublishProvider } from "./IPublishProvider";
 
 export interface IPublisher {
     
     publish(message: IMessage): void;
     setPublishProvider(publishProvider: IPublishProvider): void;
+    setAllMessagesEnabled(enabled: boolean): void;
+    setMessageTypeEnabled(type: MessageType, enabled: boolean): void;
 }
