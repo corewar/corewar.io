c94123563861693c8134eaf850156d9192b74985
dougajmcdonald <dougajmcdonald@gmail.com>
2017-12-22 18:51:16 +0000

    simple fix for dead warriors

--- simulator/Fetcher.ts
+++ simulator/Fetcher.ts
@@ -7,28 +7,42 @@ export class Fetcher implements IFetcher {
 
     public fetch(state: IState, core: ICore): IExecutionContext {
 
-        var wi = state.warriorIndex;
-        var warrior = state.warriors[wi];
+        try {
+            var wi = state.warriorIndex;
+            var warrior = state.warriors[wi];
 
-        var ti = warrior.taskIndex;
-        var task = warrior.tasks[ti];
+            // skip dead warriors
+            if(warrior.tasks.length === 0) {
+                wi = (wi + 1) % state.warriors.length;
+                warrior = state.warriors[wi]
+            }
 
-        state.warriorIndex = (wi + 1) % state.warriors.length;
-        warrior.taskIndex = (ti + 1) % warrior.tasks.length;
+            var ti = warrior.taskIndex;
+            var task = warrior.tasks[ti];
 
-        var ip = task.instructionPointer;
-        var instruction = core.executeAt(task, ip);
+            state.warriorIndex = (wi + 1) % state.warriors.length;
+            warrior.taskIndex = (ti + 1) % warrior.tasks.length;
 
-        task.instructionPointer = (ip + 1) % state.options.coresize;
-        // TODO should we instantiate an object everytime?
-        return {
-            core: core,
-            instructionPointer: ip,
-            instruction: instruction,
-            taskIndex: ti,
-            task: task,
-            warriorIndex: wi,
-            warrior: warrior
-        };
+
+            var ip = task.instructionPointer;
+            var instruction = core.executeAt(task, ip);
+            task.instructionPointer = (ip + 1) % state.options.coresize;
+
+            // TODO should we instantiate an object everytime?
+            return {
+                core: core,
+                instructionPointer: ip,
+                instruction: instruction,
+                taskIndex: ti,
+                task: task,
+                warriorIndex: wi,
+                warrior: warrior
+            };
+
+        } catch (error) {
+            debugger
+        }
+
+
     }
 }
