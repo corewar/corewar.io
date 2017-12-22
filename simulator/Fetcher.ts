f4fc284b0f4ffe1c8ac1d1f04d97faedd1b5e613
dougajmcdonald <dougajmcdonald@gmail.com>
2017-12-22 18:52:50 +0000

    cleanup

--- simulator/Fetcher.ts
+++ simulator/Fetcher.ts
@@ -7,42 +7,35 @@ export class Fetcher implements IFetcher {
 
     public fetch(state: IState, core: ICore): IExecutionContext {
 
-        try {
-            var wi = state.warriorIndex;
-            var warrior = state.warriors[wi];
+        var wi = state.warriorIndex;
+        var warrior = state.warriors[wi];
 
-            // skip dead warriors
-            if(warrior.tasks.length === 0) {
-                wi = (wi + 1) % state.warriors.length;
-                warrior = state.warriors[wi]
-            }
-
-            var ti = warrior.taskIndex;
-            var task = warrior.tasks[ti];
-
-            state.warriorIndex = (wi + 1) % state.warriors.length;
-            warrior.taskIndex = (ti + 1) % warrior.tasks.length;
-
-
-            var ip = task.instructionPointer;
-            var instruction = core.executeAt(task, ip);
-            task.instructionPointer = (ip + 1) % state.options.coresize;
-
-            // TODO should we instantiate an object everytime?
-            return {
-                core: core,
-                instructionPointer: ip,
-                instruction: instruction,
-                taskIndex: ti,
-                task: task,
-                warriorIndex: wi,
-                warrior: warrior
-            };
-
-        } catch (error) {
-            debugger
+        // skip dead warriors
+        if(warrior.tasks.length === 0) {
+            wi = (wi + 1) % state.warriors.length
+            warrior = state.warriors[wi]
         }
 
+        var ti = warrior.taskIndex;
+        var task = warrior.tasks[ti];
 
+        state.warriorIndex = (wi + 1) % state.warriors.length;
+        warrior.taskIndex = (ti + 1) % warrior.tasks.length;
+
+
+        var ip = task.instructionPointer;
+        var instruction = core.executeAt(task, ip);
+        task.instructionPointer = (ip + 1) % state.options.coresize;
+
+        // TODO should we instantiate an object everytime?
+        return {
+            core: core,
+            instructionPointer: ip,
+            instruction: instruction,
+            taskIndex: ti,
+            task: task,
+            warriorIndex: wi,
+            warrior: warrior
+        };
     }
 }
