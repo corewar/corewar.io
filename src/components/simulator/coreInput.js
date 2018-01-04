9b230d9cbf502c35325284f8e2cd170871aa5d59
dougajmcdonald <dougajmcdonald@gmail.com>
2018-01-04 20:54:33 +0000

    Display a bar for the warriors current tasks

--- src/components/simulator/coreInput.js
+++ src/components/simulator/coreInput.js
@@ -1,19 +1,50 @@
-import React from 'react'
+import React, { Component } from 'react'
+import * as PubSub from 'pubsub-js'
 import FontAwesome from 'react-fontawesome'
+import { List } from 'immutable'
 
 import './coreInput.css'
 
-const CoreInput = ({ parseResults, removeWarrior }) => (
-  <section id="coreInput">
-    {
-      parseResults && parseResults.map((result, i) => (
-        <div className="coreItem" key={`${result.metaData.name}_${i}`}>
-          <span>{result.metaData.name}</span> <FontAwesome name='times' onClick={() => removeWarrior(i)}/>
-          <div className={`inputItem coreItem_${i}`}></div>
-        </div>
-      ))
-    }
-  </section>
-)
+class CoreInput extends Component {
 
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      tasks: List()
+    }
+
+    PubSub.subscribe('TASK_COUNT', (msg, data) => {
+      const newTasks = this.state.tasks.splice(data.warriorId, 1, data.taskCount)
+      this.setState({ tasks: newTasks })
+    })
+  }
+
+  componentWillUnmount() {
+    PubSub.unsubscribe('TASK_COUNT')
+  }
+
+  render() {
+    const { parseResults, removeWarrior } = this.props
+    return <section id="coreInput">
+      {
+        parseResults && parseResults.map((result, i) => {
+          const taskCount = this.state.tasks.get(i)
+          return <div className="coreItem" key={`${result.metaData.name}_${i}`}>
+              <div className={`coreItem_${i}`}></div>
+              <span className={`itemName`}>{result.metaData.name}</span>
+              <FontAwesome name='times' onClick={() => removeWarrior(i)}/>
+              <section className={`taskCount`}>
+                <div className={`inputItem coreItem_${i}`} style={{width: parseInt(taskCount / 8000 * 100 , 10) + '%' }}>
+                </div>
+                <span>{taskCount && taskCount}</span>
+              </section>
+            </div>
+          }
+        )
+      }
+    </section>
+  }
+}
 export default CoreInput
+
