ac572083a4ad827fae44e38c6fe5dd1f26ac0bd4
dougajmcdonald <dougajmcdonald@gmail.com>
2018-01-05 13:07:54 +0000

    clean up comments

--- src/components/errorBoundary.js
+++ src/components/errorBoundary.js
@@ -2,21 +2,18 @@ import React from 'react'
 
 class ErrorBoundary extends React.Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = { hasError: false }
   }
 
   componentDidCatch(error, info) {
-    // Display fallback UI
     this.setState({ hasError: true })
-    // You can also log the error to an error reporting service
-    //logErrorToMyService(error, info)
-    throw new Error("Core has errored", error, info)
+    // this gets reported to rollbar
+    throw new Error(error, info)
   }
 
   render() {
     if (this.state.hasError) {
-      // You can render any custom fallback UI
       return <h1>This component has errored</h1>
     }
     return this.props.children
