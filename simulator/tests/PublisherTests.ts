0c75fc4b8d099e381d52b521724372dd5e6b48f9
gareththegeek <gj_williams2000@yahoo.co.uk>
2018-01-09 21:00:52 +0000

    Fix PublisherTests

--- simulator/tests/PublisherTests.ts
+++ simulator/tests/PublisherTests.ts
@@ -20,7 +20,9 @@ describe("Publisher", () => {
 
         const publisher = new Publisher([]);
 
-        const provider = sinon.stub();
+        const provider = {
+            publishSync: sinon.stub()
+        };
 
         publisher.setPublishProvider(provider);
 
@@ -68,23 +70,23 @@ describe("Publisher", () => {
 
     it("publishes all queued messages by dequeueing all strategies", () => {
 
-        const coreAccessMessage = { type: MessageType.CoreAccess, payload: {} };
-        const runProgressMessage = { type: MessageType.RunProgress, payload: {} };
-        const roundEndMessage = { type: MessageType.RoundEnd, payload: {} };
+        const coreAccessMessages = [{ type: MessageType.CoreAccess, payload: {} }];
+        const runProgressMessages = [{ type: MessageType.RunProgress, payload: {} }];
+        const roundEndMessages = [{ type: MessageType.RoundEnd, payload: {} }];
         const taskCountMessages = [
             { type: MessageType.TaskCount, payload: { a: "a" } },
             { type: MessageType.TaskCount, payload: { b: "b" } }
         ];
-        const initialiseMessage = { type: MessageType.CoreInitialise, payload: {} };
-        const roundStartMessage = { type: MessageType.RoundStart, payload: {} };
+        const initialiseMessages = [{ type: MessageType.CoreInitialise, payload: {} }];
+        const roundStartMessages = [{ type: MessageType.RoundStart, payload: {} }];
 
         const strategies = [
-            { dequeue: sinon.stub().returns(coreAccessMessage), queue: sinon.stub() },
-            { dequeue: sinon.stub().returns(runProgressMessage), queue: sinon.stub() },
-            { dequeue: sinon.stub().returns(roundEndMessage), queue: sinon.stub() },
+            { dequeue: sinon.stub().returns(coreAccessMessages), queue: sinon.stub() },
+            { dequeue: sinon.stub().returns(runProgressMessages), queue: sinon.stub() },
+            { dequeue: sinon.stub().returns(roundEndMessages), queue: sinon.stub() },
             { dequeue: sinon.stub().returns(taskCountMessages), queue: sinon.stub() },
-            { dequeue: sinon.stub().returns(initialiseMessage), queue: sinon.stub() },
-            { dequeue: sinon.stub().returns(roundStartMessage), queue: sinon.stub() }
+            { dequeue: sinon.stub().returns(initialiseMessages), queue: sinon.stub() },
+            { dequeue: sinon.stub().returns(roundStartMessages), queue: sinon.stub() }
         ];
 
         const provider = { publishSync: sinon.stub() };
@@ -94,12 +96,12 @@ describe("Publisher", () => {
 
         publisher.publish();
 
-        expect(provider.publishSync).to.have.been.calledWith("CORE_ACCESS", coreAccessMessage.payload);
-        expect(provider.publishSync).to.have.been.calledWith("RUN_PROGRESS", runProgressMessage.payload);
-        expect(provider.publishSync).to.have.been.calledWith("ROUND_END", roundEndMessage.payload);
+        expect(provider.publishSync).to.have.been.calledWith("CORE_ACCESS", coreAccessMessages[0].payload);
+        expect(provider.publishSync).to.have.been.calledWith("RUN_PROGRESS", runProgressMessages[0].payload);
+        expect(provider.publishSync).to.have.been.calledWith("ROUND_END", roundEndMessages[0].payload);
         expect(provider.publishSync).to.have.been.calledWith("TASK_COUNT", taskCountMessages[0].payload);
         expect(provider.publishSync).to.have.been.calledWith("TASK_COUNT", taskCountMessages[1].payload);
-        expect(provider.publishSync).to.have.been.calledWith("CORE_INITIALISE", initialiseMessage.payload);
-        expect(provider.publishSync).to.have.been.calledWith("ROUND_START", roundStartMessage.payload);
+        expect(provider.publishSync).to.have.been.calledWith("CORE_INITIALISE", initialiseMessages[0].payload);
+        expect(provider.publishSync).to.have.been.calledWith("ROUND_START", roundStartMessages[0].payload);
     });
 });
