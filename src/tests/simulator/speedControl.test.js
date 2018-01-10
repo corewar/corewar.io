330c17686bb131a3ba8ff3a6e0e4f605ecb26968
dougajmcdonald <dougajmcdonald@gmail.com>
2018-01-10 22:15:59 +0000

    complete speed control tests

--- src/tests/simulator/speedControl.test.js
+++ src/tests/simulator/speedControl.test.js
@@ -39,19 +39,33 @@ it('renders one list item per processRate', () => {
 
 it('fires the clickHandler when a li is clicked', () => {
 
-    const clickHandler = sinon.spy()
+  const clickHandler = sinon.spy()
 
-    const props = {
-      handleClick: clickHandler,
-      processRates: [
-        1, 2, 3, 4
-      ]
-    }
+  const props = {
+    handleClick: clickHandler,
+    processRates: [
+      1, 2, 3, 4
+    ]
+  }
 
-    const wrapper = mount(<SpeedControl {...props}/>)
+  const wrapper = mount(<SpeedControl {...props}/>)
 
-    wrapper.find('li').first().simulate('click')
+  wrapper.find('li').first().simulate('click')
 
-    expect(clickHandler.calledOnce)
-  });
+  expect(clickHandler.calledOnce)
+})
+
+it('applies the active class to the current process rate', () => {
+
+  const props = {
+    processRate: 2,
+    processRates: [
+      1, 2, 3, 4
+    ]
+  }
+
+  const wrapper = mount(<SpeedControl {...props}/>)
+
+  expect(wrapper.find('.optionDropdown ul li').at(1).props().className).to.equal(`active`)
+})
 
