23473c14fd6549df020d5f825cc883637e7586d1
dougajmcdonald <dougajmcdonald@gmail.com>
2018-01-23 20:15:35 +0000

    remove old tests

--- src/containers/app/index.js
+++ src/containers/app/index.js
@@ -3,14 +3,12 @@ import { Route } from 'react-router-dom'
 import styled from 'styled-components'
 
 import SiteHeader from './../../components/app/siteHeader'
-import DebugInterface from './../../components/app/debugInterface'
 import ParserInterface from './../../components/app/parserInterface'
 import OutputInterface from '../../components/app/outputInterface'
 
 import { colour, space } from '../../styles/theme'
 
 const RootGrid = styled.div`
-
   display: grid;
   grid-template-columns: 1fr;
   grid-template-rows: 48px 1fr;
@@ -26,8 +24,7 @@ const App = () => (
   <RootGrid>
     <SiteHeader isAuthenticated={false}/>
     <main>
-      <Route exact path="/" component={ParserInterface} />
-      <Route exact path="/parser" component={DebugInterface} />
+      <Route exact path='/' component={ParserInterface} />
       <Route exact path='/src' component={ParserInterface} />
       <Route exact path='/output' component={OutputInterface} />
       <Route exact path='/core' component={ParserInterface} />
