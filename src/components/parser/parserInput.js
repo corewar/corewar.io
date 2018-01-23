fffa3bf3b4878094b7a60ebb82abd47db254ae07
dougajmcdonald <dougajmcdonald@gmail.com>
2018-01-23 20:09:08 +0000

    extract source code mixin, remove components which aren't needed

--- src/components/parser/parserInput.js
+++ src/components/parser/parserInput.js
@@ -3,37 +3,10 @@ import styled from 'styled-components'
 
 import { colour, space, font } from '../../styles/theme'
 
+import { sourceCode } from '../../styles/sourceCode'
+
 const StyledTextarea = styled.textarea`
-  font-family: ${font.code};
-  line-height: 1.2em;
-  font-size: ${font.base};
-  background-color: transparent;
-  margin: ${space.m};
-  width: calc(100% - ${space.m} - ${space.m});
-  height: calc(100% - ${space.m} - ${space.m});
-  border: none;
-  caret-color: ${colour.white};
-  color: ${colour.white};
-  resize: none;
-
-  &:focus {
-    outline: none;
-  }
-
-  ::-webkit-scrollbar-track {
-    background-color: transparent;
-  }
-
-  ::-webkit-scrollbar {
-    width: ${space.xs};
-    background-color: transparent;
-  }
-
-  ::-webkit-scrollbar-thumb {
-    border-radius: ${space.xs};
-    background-color: ${colour.blue};
-  }
-
+  ${sourceCode}
 `
 
 const ParserInput = ({ redcode, handleChange }) => (
--- src/components/parser/parserOutput.js
+++ src/components/parser/parserOutput.js
@@ -2,44 +2,17 @@ import React from 'react'
 import styled from 'styled-components'
 
 import { colour, space, font } from '../../styles/theme'
+import { sourceCode } from '../../styles/sourceCode'
 
 const StyledPre = styled.pre`
-  font-family: ${font.code};
-  font-size: ${font.base};
-  line-height: 1.2em;
-  background-color: transparent;
-  padding: ${space.m};
-  width: calc(100% - ${space.m} - ${space.m});
-  height: calc(100% - ${space.m} - ${space.m});
-  border: none;
-  caret-color: ${colour.white};
-  color: ${colour.white};
-  resize: none;
-  overflow-y: scroll;
-
-  &:focus {
-    outline: none;
-  }
-
-  ::-webkit-scrollbar-track {
-    background-color: transparent;
-  }
-
-  ::-webkit-scrollbar {
-    width: ${space.xs};
-    background-color: transparent;
-  }
-
-  ::-webkit-scrollbar-thumb {
-    border-radius: ${space.xs};
-    background-color: ${colour.blue};
-  }
-
+  ${sourceCode}
 `
 
 const ParserOutput = ({ parseResult }) => (
   <StyledPre>
+    <code>
     {parseResult ? parseResult.warrior : 'awaiting redcode'}
+    </code>
   </StyledPre>
 )
 
