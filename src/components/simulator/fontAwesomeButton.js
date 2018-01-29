d7ada7f4c378e7207092cbc300bfa6a09833982b
dougajmcdonald <dougajmcdonald@gmail.com>
2018-01-29 20:23:22 +0000

    Add speed control back in

--- src/components/simulator/fontAwesomeButton.js
+++ src/components/simulator/fontAwesomeButton.js
@@ -7,8 +7,7 @@ import { colour, font } from '../../styles/theme'
 const Container = styled.div.attrs({
   onClick: props => props.enabled ? props.handleClick : null
 })`
-
-  display: ${props => !props.visible && `none`};
+  ${props => !props.visible && `display: none;`};
   color: ${props => props.enabled ? `${colour.white}` : `${colour.grey}`};
   font-size: ${font.small};
   width: 100%;
