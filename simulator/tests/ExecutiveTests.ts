48f186a41ad83cce20d55a51fd56b1078a3edd42
gareththegeek <gj_williams2000@yahoo.co.uk>
2018-01-29 22:50:56 +0000

    Implement mathematical operand tests

--- simulator/tests/ExecutiveTests.ts
+++ simulator/tests/ExecutiveTests.ts
@@ -46,7 +46,49 @@ describe("Executive", () => {
         { i: "MOV.BA", a: "NOP.A $1, $2", b: "DAT.F #3, #4", e: "DAT.F #2, #4" },
         { i: "MOV.F" , a: "NOP.A $1, $2", b: "DAT.F #3, #4", e: "DAT.F #1, #2" },
         { i: "MOV.X" , a: "NOP.A $1, $2", b: "DAT.F #3, #4", e: "DAT.F #2, #1" },
-        { i: "MOV.I" , a: "NOP.A $1, $2", b: "DAT.F #3, #4", e: "NOP.A $1, $2" }
+        { i: "MOV.I" , a: "NOP.A $1, $2", b: "DAT.F #3, #4", e: "NOP.A $1, $2" },
+
+        { i: "ADD.A" , a: "NOP.A $1, $2", b: "DAT.F #3, #4", e: "DAT.F #4, #4" },
+        { i: "ADD.B" , a: "NOP.A $1, $2", b: "DAT.F #3, #4", e: "DAT.F #3, #6" },
+        { i: "ADD.AB", a: "NOP.A $1, $2", b: "DAT.F #3, #4", e: "DAT.F #3, #5" },
+        { i: "ADD.BA", a: "NOP.A $1, $2", b: "DAT.F #3, #4", e: "DAT.F #5, #4" },
+        { i: "ADD.F" , a: "NOP.A $1, $2", b: "DAT.F #3, #4", e: "DAT.F #4, #6" },
+        { i: "ADD.X" , a: "NOP.A $1, $2", b: "DAT.F #3, #4", e: "DAT.F #5, #5" },
+        { i: "ADD.I" , a: "NOP.A $1, $2", b: "DAT.F #3, #4", e: "DAT.F #4, #6" },
+
+        { i: "SUB.A" , a: "NOP.A $1, $2", b: "DAT.F #3, #4", e: "DAT.F #2, #4" },
+        { i: "SUB.B" , a: "NOP.A $1, $2", b: "DAT.F #3, #4", e: "DAT.F #3, #2" },
+        { i: "SUB.AB", a: "NOP.A $1, $2", b: "DAT.F #3, #4", e: "DAT.F #3, #3" },
+        { i: "SUB.BA", a: "NOP.A $1, $2", b: "DAT.F #3, #4", e: "DAT.F #1, #4" },
+        { i: "SUB.F" , a: "NOP.A $1, $2", b: "DAT.F #3, #4", e: "DAT.F #2, #2" },
+        { i: "SUB.X" , a: "NOP.A $1, $2", b: "DAT.F #3, #4", e: "DAT.F #1, #3" },
+        { i: "SUB.I" , a: "NOP.A $1, $2", b: "DAT.F #3, #4", e: "DAT.F #2, #2" },
+
+        { i: "MUL.A" , a: "NOP.A $1, $2", b: "DAT.F #3, #4", e: "DAT.F #3, #4" },
+        { i: "MUL.B" , a: "NOP.A $1, $2", b: "DAT.F #3, #4", e: "DAT.F #3, #8" },
+        { i: "MUL.AB", a: "NOP.A $1, $2", b: "DAT.F #3, #4", e: "DAT.F #3, #4" },
+        { i: "MUL.BA", a: "NOP.A $1, $2", b: "DAT.F #3, #4", e: "DAT.F #6, #4" },
+        { i: "MUL.F" , a: "NOP.A $1, $2", b: "DAT.F #3, #4", e: "DAT.F #3, #8" },
+        { i: "MUL.X" , a: "NOP.A $1, $2", b: "DAT.F #3, #4", e: "DAT.F #6, #4" },
+        { i: "MUL.I" , a: "NOP.A $1, $2", b: "DAT.F #3, #4", e: "DAT.F #3, #8" },
+
+        { i: "DIV.A" , a: "NOP.A $2, $4", b: "DAT.F #8, #12", e: "DAT.F #4, #12" },
+        { i: "DIV.B" , a: "NOP.A $2, $4", b: "DAT.F #8, #12", e: "DAT.F #8, #3" },
+        { i: "DIV.AB", a: "NOP.A $2, $4", b: "DAT.F #8, #12", e: "DAT.F #8, #6" },
+        { i: "DIV.BA", a: "NOP.A $2, $4", b: "DAT.F #8, #12", e: "DAT.F #2, #12" },
+        { i: "DIV.F" , a: "NOP.A $2, $4", b: "DAT.F #8, #12", e: "DAT.F #4, #3" },
+        { i: "DIV.X" , a: "NOP.A $2, $4", b: "DAT.F #8, #12", e: "DAT.F #2, #6" },
+        { i: "DIV.I" , a: "NOP.A $2, $4", b: "DAT.F #8, #12", e: "DAT.F #4, #3" },
+        //TODO test rounding
+        //TODO test divide by zero in separate test
+
+        { i: "MOD.A" , a: "NOP.A $2, $4", b: "DAT.F #9, #14", e: "DAT.F #1, #14" },
+        { i: "MOD.B" , a: "NOP.A $2, $4", b: "DAT.F #9, #14", e: "DAT.F #9, #2" },
+        { i: "MOD.AB", a: "NOP.A $2, $4", b: "DAT.F #9, #14", e: "DAT.F #9, #0" },
+        { i: "MOD.BA", a: "NOP.A $2, $4", b: "DAT.F #9, #14", e: "DAT.F #1, #14" },
+        { i: "MOD.F" , a: "NOP.A $2, $4", b: "DAT.F #9, #14", e: "DAT.F #1, #2" },
+        { i: "MOD.X" , a: "NOP.A $2, $4", b: "DAT.F #9, #14", e: "DAT.F #1, #0" },
+        { i: "MOD.I" , a: "NOP.A $2, $4", b: "DAT.F #9, #14", e: "DAT.F #1, #2" }
     ], (context: IExecutionContext, expectation: string) => {
 
         it("correctly executes " + TestHelper.instructionToString(context.instruction), () => {
