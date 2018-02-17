1cc71ec6d12c26d6135687b128a96047bbf2de23
dougajmcdonald <dougajmcdonald@gmail.com>
2018-02-17 12:15:40 +0000

    implement a throttle function and apply to resize event in the core

---
+++ src/helpers/throttle.js
@@ -0,0 +1,18 @@
+const throttle = (callback, wait, context = this) => {
+  let timeout = null
+  let callbackArgs = null
+
+  const later = () => {
+    callback.apply(context, callbackArgs)
+    timeout = null
+  }
+
+  return () => {
+    if (!timeout) {
+      callbackArgs = arguments
+      timeout = setTimeout(later, wait)
+    }
+  }
+}
+
+export default throttle
