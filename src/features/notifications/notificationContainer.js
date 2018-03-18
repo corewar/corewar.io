e63bde8af0642d8df5a8bd66a83572f1d1cc89d6
dougajmcdonald <dougajmcdonald@gmail.com>
2018-03-18 13:04:56 +0000

    animate toasts and colour better

--- src/features/notifications/notificationContainer.js
+++ src/features/notifications/notificationContainer.js
@@ -1,29 +1,55 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import styled from 'styled-components'
+import transition from 'styled-transition-group'
+import { TransitionGroup } from 'react-transition-group'
 
 import { space, colour } from '../common/theme'
+import { media } from '../common/mediaQuery'
 
-const NotificationWrapper = styled.div`
-
-  ${props => props.notifications && props.notifications.length > 0 ? `display: block;` : `display: none;`}
+const SlideInOut = transition.div`
+  ${props => `bottom: calc(${space.controls} + ${space.s} + (${props.index} * 50px));`}
 
   position: absolute;
-  bottom: ${space.controls};
-  right: 0;
-  width: 50%;
+
+  width: 300px;
   max-width: 300px;
 
-
-  background-color: ${colour.lightbg};
-  color: ${colour.blue};
+  background-color: ${colour.notification};
+  color: ${colour.white};
   text-align: center;
   padding: ${space.s};
   margin-bottom: ${space.s};
   z-index: 100;
+
+  ${props => media.tablet && `right: -300px;`};
+  ${props => media.designer && `right: -316px;`};
+  transition: 500ms;
+
+  &:enter {
+    ${props => media.tablet && `right: -300px;`};
+    ${props => media.designer && `right: -316px;`};
+    transition: 500ms;
+  }
+  &:enter-active {
+    ${props => media.tablet && `right: 0;`};
+    ${props => media.designer && `right: -${space.m};`};
+    transition: 500ms;
+  }
+  &:exit {
+    ${props => media.tablet && `right: 0;`};
+    ${props => media.designer && `right: -${space.m};`};
+    transition: 500ms;
+  }
+  &:exit-active {
+    ${props => media.tablet && `right: -300px;`};
+    ${props => media.designer && `right: -316px;`};
+    transition: 500ms;
+  }
+
 `
 
-const NotficationItem = styled.div`
+const NotificationItem = styled.div`
   margin-bottom: ${space.s};
   margin-top: ${space.s};
   line-height: ${space.m};
@@ -31,15 +57,20 @@ const NotficationItem = styled.div`
 `
 
 const NotificationContainer = ({ notifications }) => (
-
-  <NotificationWrapper notifications={notifications}>
+  <TransitionGroup>
     {notifications && notifications.map((content, i) => (
-      <NotficationItem key={`${content}_${i}`}>
-        {content}
-      </NotficationItem>
+      <SlideInOut
+        key={`${content}_${i}`}
+        index={i}
+        in={true}
+        timeout={2000}
+        unmountOnExit>
+          <NotificationItem>
+            {content}
+          </NotificationItem>
+        </SlideInOut>
     ))}
-  </NotificationWrapper>
-
+  </TransitionGroup>
 )
 
 const mapStateToProps = state => ({
