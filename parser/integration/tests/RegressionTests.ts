7acace716ef055d42e3e31a4f4f2f60f35a80e40
gareththegeek <gj_williams2000@yahoo.co.uk>
2018-03-24 14:12:02 +0000

    Integration test to reproduce issue

---
+++ parser/integration/tests/RegressionTests.ts
@@ -0,0 +1,15 @@
+import { expect } from "chai";
+import { TestHelper } from  "./TestHelper";
+import { Standard } from "./../../interface/IParseOptions";
+import { MessageType } from "../../interface/IMessage";
+
+describe("Regression Tests",() => {
+
+    it("correctly handles an instruction with no modifier",() => {
+        const actual = TestHelper.testWarriorParse("MOV", Standard.ICWS94draft);
+
+        expect(actual.messages.length).to.be.equal(1);
+        expect(actual.messages[0].text).to.be.equal("Expected number, got end of line");
+        expect(actual.messages[0].type).to.be.equal(MessageType.Error);
+    });
+});
