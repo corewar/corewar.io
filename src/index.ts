686c839088edd6afb0314c159f61417f260fba25
gareththegeek <gareththegeek@domain.com>
2020-01-28 17:43:05 +0000

    Basic parse endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,18 @@
 import 'reflect-metadata'
+import ParseResolver from './resolvers/ParseResolver'
+import { buildSchema } from 'type-graphql'
+import { ApolloServer } from 'apollo-server'
 
-console.log('It\'s alive')
+(async () => {
+    const schema = await buildSchema({
+        resolvers: [ParseResolver]
+    })
+
+    const server = new ApolloServer({
+        schema
+    })
+
+    server.listen().then(({ url }) => {
+        console.log(`🚀 Server ready at ${url}`)
+    })
+})()
---
+++ src/resolvers/ParseResolver.ts
@@ -0,0 +1,11 @@
+import { Resolver, Query, Arg } from 'type-graphql';
+import ParseResult from '../parse/ParseResult';
+import { corewar } from 'corewar';
+
+@Resolver(ParseResult)
+export default class ParseResolver {
+    @Query(returns => ParseResult)
+    async parse(@Arg('redcode') redcode: string) {
+        return corewar.parse(redcode)
+    }
+}
