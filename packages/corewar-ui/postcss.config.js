507fe2bf75416fa426ad91626146757cbb320af8
Doug McDonald <dougajmcdonald@gmail.com>
2020-02-24 22:34:59 +0000

    Fix tailwind config, example of component migration

--- packages/corewar-ui/postcss.config.js
+++ packages/corewar-ui/postcss.config.js
@@ -1,4 +1,3 @@
-const tailwindcss = require('tailwindcss')
-module.exports = {
-  plugins: [tailwindcss('./tailwind.js'), require('autoprefixer')]
-}
+module.exports = () => ({
+  plugins: [require('tailwindcss'), require('autoprefixer')]
+})
---
+++ packages/corewar-ui/tailwind.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  theme: {
+    fontFamily: {
+      display: ['Inter'],
+      body: ['Inter'],
+      code: ['Anonymous Pro']
+    },
+    extend: {
+      colors: {
+        blue: '#56CCF2',
+        coral: '#EB5757',
+        white: '#f2f2f2',
+        darkbg: '#131518',
+        defaultbg: '#1A1D22',
+        lightbg: '#292C33',
+        bluegrey: '#B5BAD0',
+        lightgrey: '#E0E0E0',
+        grey: '#828282',
+        error: '#EB5757',
+        warning: '#F2C94C',
+        info: '#56CCF2',
+        success: '#6FCF97'
+      }
+    }
+  },
+  variants: {},
+  plugins: []
+}
