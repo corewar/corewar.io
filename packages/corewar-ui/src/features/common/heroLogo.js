6894ee9e175c805a13f216af9595a34bcea0018b
Doug McDonald <dougajmcdonald@gmail.com>
2020-02-26 21:28:02 +0000

    remove duplication

--- packages/corewar-ui/src/features/common/heroLogo.js
+++ packages/corewar-ui/src/features/common/heroLogo.js
@@ -1,6 +1,12 @@
 import React from 'react'
 import Octicon from 'react-octicon'
 
+const features = [
+  { icon: 'rocket', heading: 'Play corewar' },
+  { icon: 'device-mobile', heading: 'On any device' },
+  { icon: 'megaphone', heading: 'In new ways' }
+]
+
 const Feature = ({ children }) => (
   <div className="flex flex-col items-center mt-2 md:mt-16 lg:border-r-2 border-lightbg">
     {children}
@@ -8,8 +14,8 @@ const Feature = ({ children }) => (
 )
 
 const HeroLogo = () => (
-  <section className="flex flex-row justify-center items-center font-thin mt-4 mx-16">
-    <div className="flex flex-row justify-center items-center">
+  <section className="flex justify-center items-center font-thin mt-4 mx-16">
+    <div className="flex justify-center items-center">
       <h1 className="text-5xl md:text-7xl font-hairline">
         corewar<span className="font-light">.io</span>
       </h1>
@@ -18,19 +24,13 @@ const HeroLogo = () => (
         className="text-2xl md:text-3xl text-blue fill-current ml-8 pt-4"
       />
     </div>
-    <section className="hidden md:flex flex-row flex-wrap justify-center ml-16">
-      <Feature>
-        <Octicon name="rocket" className="text-xl text-blue fill-current" />
-        <h3 className="m-4 font-light text-lightgrey text-center">Play corewar</h3>
-      </Feature>
-      <Feature>
-        <Octicon name="device-mobile" className="text-xl text-blue fill-current" />
-        <h3 className="m-4 font-light text-lightgrey text-center">On any device</h3>
-      </Feature>
-      <Feature>
-        <Octicon name="megaphone" className="text-xl text-blue fill-current" />
-        <h3 className="m-4 font-light text-lightgrey text-center">In new ways</h3>
-      </Feature>
+    <section className="hidden md:flex flex-wrap justify-center ml-16">
+      {features.map(feature => (
+        <Feature>
+          <Octicon name={feature.icon} className="text-xl text-blue fill-current" />
+          <h3 className="m-4 font-light text-lightgrey text-center">{feature.heading}</h3>
+        </Feature>
+      ))}
     </section>
   </section>
 )
