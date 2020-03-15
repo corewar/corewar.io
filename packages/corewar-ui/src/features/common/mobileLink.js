23a7af251e356ab2f37632436c33c61d15b7eaaa
Doug McDonald <dougajmcdonald@gmail.com>
2020-03-15 12:31:26 +0000

    Extract links into components

---
+++ packages/corewar-ui/src/features/common/mobileLink.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default () => ({ route, children }) => (
+  <a
+    className="block md:inline-block mt-4 md:mt-0 md:ml-6 no-underline text-white font-display hover:no-underline cursor-pointer"
+    href={route}
+  >
+    {children}
+  </a>
+)
--- packages/corewar-ui/src/features/common/siteNav.js
+++ packages/corewar-ui/src/features/common/siteNav.js
@@ -1,24 +1,8 @@
 import React, { useState } from 'react'
 
 import Logo from '../topbar/logo'
-
-const Link = ({ route, children }) => (
-  <a
-    className="text-white font-display mr-2 ml-2 lg:ml-10 lg:mr-10 leading-loose border-b-2 border-transparent hover:border-blue hover:no-underline cursor-pointer"
-    href={route}
-  >
-    {children}
-  </a>
-)
-
-const MobileLink = ({ route, children }) => (
-  <a
-    className="block md:inline-block mt-4 md:mt-0 md:ml-6 no-underline text-white font-display hover:no-underline cursor-pointer"
-    href={route}
-  >
-    {children}
-  </a>
-)
+import Link from './link'
+import MobileLink from './mobileLink'
 
 const links = [
   { route: `/app/editor/src`, text: `play` },
