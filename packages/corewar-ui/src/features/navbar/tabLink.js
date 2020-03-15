199d8382a0c03ee91ac39d71f477e6a959384850
Doug McDonald <dougajmcdonald@gmail.com>
2020-03-15 20:40:40 +0000

    moe tweaks

--- packages/corewar-ui/src/features/navbar/tabLink.js
+++ packages/corewar-ui/src/features/navbar/tabLink.js
@@ -6,9 +6,11 @@ import { colour, space } from '../common/theme'
 
 const BaseLink = styled(NavLink)`
   color: ${colour.grey};
-  padding-top: ${space.m};
-  height: calc(${space.header} - ${space.m});
+  height: 100%;
   width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
   text-decoration: none;
   border-right: 1px solid ${colour.defaultbg};
   &:hover {
