56e00926c6420c8b30c51fdd800b79df8b5d5a97
Doug McDonald <dougajmcdonald@gmail.com>
2020-03-15 14:00:52 +0000

    ally

--- packages/corewar-ui/src/features/topbar/logo.js
+++ packages/corewar-ui/src/features/topbar/logo.js
@@ -5,7 +5,7 @@ import CorewarLogo from '../../img/corewar-logo.png'
 
 const Logo = ({ siteName, siteDomain }) => (
   <a className="flex" href={`/`}>
-    <img className="m-4 w-12" src={CorewarLogo} />
+    <img className="m-4 w-12" src={CorewarLogo} alt="corewar-logo" />
     <div className="font-body text-xl text-blue font-light leading-5 pl-4 pt-2 cursor-pointer">
       {siteName}
       <span className="font-medium text-white">{siteDomain}</span>
