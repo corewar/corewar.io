119a911f35c482767f3b9bb0bf449c5ee535d86e
Doug McDonald <dougajmcdonald@gmail.com>
2020-03-15 13:55:28 +0000

    clean up logo component

--- packages/corewar-ui/src/features/common/link.js
+++ packages/corewar-ui/src/features/common/link.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default ({ route, children }) => (
   <a
-    className="text-white font-display mr-2 ml-2 lg:ml-10 lg:mr-10 leading-loose border-b-2 border-transparent hover:border-blue hover:no-underline cursor-pointer"
+    className="text-white font-display mr-2 ml-2 lg:ml-6 lg:mr-6 leading-loose border-b-2 border-transparent hover:border-blue hover:no-underline cursor-pointer"
     href={route}
   >
     {children}
--- packages/corewar-ui/src/features/topbar/logo.js
+++ packages/corewar-ui/src/features/topbar/logo.js
@@ -1,30 +1,16 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import styled from 'styled-components'
-
-import { space } from '../common/theme'
 
 import CorewarLogo from '../../img/corewar-logo.png'
 
-const Wrapper = styled.a`
-  display: grid;
-  grid-template-columns: ${space.header} 1fr;
-`
-
-const LogoImage = styled.img`
-  margin: ${space.s};
-  width: calc(${space.header} - ${space.ss} - ${space.s});
-  height: calc(${space.header} - ${space.s} - ${space.s});
-`
-
 const Logo = ({ siteName, siteDomain }) => (
-  <Wrapper href={`/`}>
-    <LogoImage src={CorewarLogo} />
+  <a className="flex" href={`/`}>
+    <img className="m-4 w-12" src={CorewarLogo} />
     <div className="font-body text-xl text-blue font-light leading-5 pl-4 pt-2 cursor-pointer">
       {siteName}
       <span className="font-medium text-white">{siteDomain}</span>
     </div>
-  </Wrapper>
+  </a>
 )
 
 Logo.propTypes = {
