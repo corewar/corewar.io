e479e038922a0e6d041f3a66d6bb5f8df4869420
gareththegeek <gj_williams2000@yahoo.co.uk>
2020-06-07 19:39:33 +0100

    Implement basic create and get hills servless functions

--- packages/corewar-hills-service/createHill/index.ts
+++ packages/corewar-hills-service/createHill/index.ts
@@ -1,9 +1,27 @@
 import { AzureFunction, Context } from '@azure/functions'
 import Repository from 'corewar-repository'
+import { IOptions } from 'corewar'
+import uuid from 'uuid/v1'
 
-const serviceBusTopicTrigger: AzureFunction = async function(context: Context, message: any): Promise<void> {
-    //context.log('ServiceBus topic trigger function processed message', mySbMsg);
-    
+interface ICreateHillMessage {
+    body: {
+        rules: {
+            rounds: number
+            size: number
+            options: IOptions
+        }
+    }
+}
+
+const serviceBusTopicTrigger: AzureFunction = async function(_: Context, message: ICreateHillMessage): Promise<void> {
+    const repo = new Repository('hills-db', 'hills')
+    const { rules } = message.body
+
+    await repo.upsert({
+        id: uuid(),
+        rules,
+        warriors: []
+    })
 }
 
 export default serviceBusTopicTrigger
