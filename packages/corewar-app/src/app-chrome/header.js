dbd83edcc0d6cd41101cc465f2ccd26059db0956
Doug McDonald <dougajmcdonald@gmail.com>
2020-07-25 18:49:27 +0100

    add home link, for usability (a7y) -tm

--- packages/corewar-app/src/app-chrome/header.js
+++ packages/corewar-app/src/app-chrome/header.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import { ReactComponent as Logo } from '../img/logo.svg'
+import { Link } from 'react-router-dom'
 
 const Header = () => (
-  <header className="w-full h-12 flex flex-row items-center">
-    <Logo title="corewar logo" />
-    <span className="ml-4 text-lg">
-      <span className="font-medium">corewar</span>.io
-    </span>
+  <header className="w-full h-12 pl-4 flex flex-row items-center">
+    <Link to="/" className="flex">
+      <Logo title="corewar logo" className="w-6 h-6" />
+      <span className="ml-4 text-lg">
+        <span className="font-medium">corewar</span>.io
+      </span>
+    </Link>
   </header>
 )
 
