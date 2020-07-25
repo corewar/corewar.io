60ab25284324c5cc3e3d5bd271a795eaaa3ed958
Doug McDonald <dougajmcdonald@gmail.com>
2020-07-25 15:28:15 +0100

    add lint to *.js, comment out lint failures on editor

--- packages/corewar-app/src/features/files/code-editor.js
+++ packages/corewar-app/src/features/files/code-editor.js
@@ -1,24 +1,24 @@
 import React, { useRef, useState } from 'react'
-import Editor from '@monaco-editor/react'
+//import Editor from '@monaco-editor/react'
 
-const options = {
-  minimap: {
-    enabled: false
-  }
-}
+// const options = {
+//   minimap: {
+//     enabled: false
+//   }
+// }
 
 const CodeEditor = () => {
-  const [isEditorReady, setIsEditorReady] = useState(false)
-  const valueGetter = useRef()
+  // const [isEditorReady, setIsEditorReady] = useState(false)
+  // const valueGetter = useRef()
 
-  function handleEditorDidMount(_valueGetter) {
-    setIsEditorReady(true)
-    valueGetter.current = _valueGetter
-  }
+  // function handleEditorDidMount(_valueGetter) {
+  //   setIsEditorReady(true)
+  //   valueGetter.current = _valueGetter
+  // }
 
-  function handleShowValue() {
-    alert(valueGetter.current())
-  }
+  // function handleShowValue() {
+  //   alert(valueGetter.current())
+  // }
 
   return (
     <section className="flex w-full p-2 rounded-lg rounded-tl-none bg-gray-700 text-gray-100">
