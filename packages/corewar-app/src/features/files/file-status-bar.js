77a2742042cad2bed554234542416237920f977e
Doug McDonald <dougajmcdonald@gmail.com>
2020-08-02 15:16:06 +0100

    add delete option

--- packages/corewar-app/src/features/files/file-status-bar.js
+++ packages/corewar-app/src/features/files/file-status-bar.js
@@ -1,7 +1,33 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
+import React, { useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 import { ReactComponent as SuccessIcon } from '../../img/icons/checkmark-outline.svg'
 import { ReactComponent as MenuIcon } from '../../img/icons/ellipsis-horizontal-outline.svg'
+import { deleteFile } from './actions'
+
+const FileSettings = ({ currentFileId }) => {
+  const [menuOpen, toggleMenu] = useState(false)
+  const dispatch = useDispatch()
+  return (
+    <div className="relative">
+      <button className="mr-2" onClick={() => toggleMenu(!menuOpen)}>
+        <MenuIcon />
+      </button>
+      {menuOpen ? (
+        <ul className="absolute top-0 right-0 mt-8 w-40 p-2 text-base rounded-lg bg-gray-800">
+          <li
+            className="py-1 px-2 w-full cursor-pointer hover:bg-gray-700 hover:text-red-100 rounded-lg font-normal"
+            onClick={() => {
+              dispatch(deleteFile(currentFileId))
+              toggleMenu(!menuOpen)
+            }}
+          >
+            Delete file
+          </li>
+        </ul>
+      ) : null}
+    </div>
+  )
+}
 
 const ParseResult = () => {
   const { currentFile } = useSelector(state => state.file)
@@ -27,9 +53,7 @@ const ParseResult = () => {
             </>
           )}
         </div>
-        <button className="mr-2">
-          <MenuIcon />
-        </button>
+        <FileSettings currentFileId={currentFile.data.id} />
       </div>
       <div>
         {currentFile.messages && currentFile.messages.length > 0 ? (
