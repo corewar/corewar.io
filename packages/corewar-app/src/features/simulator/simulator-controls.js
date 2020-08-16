e2ba2f1c246fc9604cb986c67bbafa7e0ec309f6
Doug McDonald <dougajmcdonald@gmail.com>
2020-08-16 10:29:02 +0100

    Update simulator control logic, 90% there

--- packages/corewar-app/src/features/simulator/simulator-controls.js
+++ packages/corewar-app/src/features/simulator/simulator-controls.js
@@ -1,30 +1,54 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { ReactComponent as PlayIcon } from '../../img/icons/play-circle-outline.svg'
+import { ReactComponent as PauseIcon } from '../../img/icons/pause-circle-outline.svg'
 import { ReactComponent as StepIcon } from '../../img/icons/play-skip-forward-outline.svg'
 import { ReactComponent as ReloadIcon } from '../../img/icons/refresh-outline.svg'
 import { ReactComponent as SettingsIcon } from '../../img/icons/settings-outline.svg'
 import SimulatorSettings from './simulator-settings'
-import { init, step, run, pause, toggleSettings } from './actions'
+import { init, step, run, pause } from './actions'
+import { getSimulatorState } from './reducer'
+
+const SimulatorButton = ({ enabled = true, visible = true, clickHandler, children }) =>
+  visible ? (
+    <button
+      className={`${enabled ? `cursor-pointer` : `text-gray-400 cursor-not-allowed`}`}
+      onClick={enabled ? clickHandler : null}
+    >
+      {children}
+    </button>
+  ) : null
 
 const SimulatorControls = () => {
   const dispatch = useDispatch()
+  const { isRunning, isInitialised } = useSelector(getSimulatorState)
   return (
     <div className="h-16 max-w-core mt-2 flex justify-evenly items-center text-gray-100">
-      <PlayIcon
-        className="stroke-current h-12 w-12 cursor-pointer"
-        onClick={() => dispatch(run())}
-      ></PlayIcon>
-      <StepIcon
-        className="stroke-current h-6 w-6 cursor-pointer"
-        onClick={() => dispatch(step())}
-      ></StepIcon>
-      <ReloadIcon
-        className="stroke-current h-6 w-6 cursor-pointer"
-        onClick={() => dispatch(init())}
-      ></ReloadIcon>
+      <SimulatorButton
+        clickHandler={() => dispatch(run())}
+        visible={!isRunning}
+        enabled={isInitialised}
+      >
+        <PlayIcon className={`stroke-current h-12 w-12`}></PlayIcon>
+      </SimulatorButton>
+
+      <SimulatorButton
+        clickHandler={() => dispatch(pause())}
+        visible={isRunning}
+        enabled={isRunning}
+      >
+        <PauseIcon className={`stroke-current h-12 w-12`}></PauseIcon>
+      </SimulatorButton>
+
+      <SimulatorButton clickHandler={() => dispatch(step())} enabled={isInitialised && !isRunning}>
+        <StepIcon className="stroke-current h-6 w-6"></StepIcon>
+      </SimulatorButton>
+
+      <SimulatorButton clickHandler={() => dispatch(init())} enabled={isInitialised}>
+        <ReloadIcon className="stroke-current h-6 w-6"></ReloadIcon>
+      </SimulatorButton>
+
       <SimulatorSettings />
-      {/* <SpeedIcon className="stroke-current h-6 w-6"></SpeedIcon> */}
       <SettingsIcon className="stroke-current h-6 w-6"></SettingsIcon>
     </div>
   )
