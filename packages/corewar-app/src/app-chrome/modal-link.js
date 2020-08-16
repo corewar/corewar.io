2b729ba113074c61c64a757c847bcefbe9f26b7b
Doug McDonald <dougajmcdonald@gmail.com>
2020-08-16 12:59:58 +0100

    add simulator settings, basic

--- packages/corewar-app/src/app-chrome/modal-link.js
+++ packages/corewar-app/src/app-chrome/modal-link.js
@@ -7,7 +7,6 @@ const ModalLink = ({ id, children }) => (
       pathname: `/modal/${id}`,
       state: { modal: true }
     }}
-    className="py-2 px-8 rounded-full mr-4 bg-gray-700 border border-transparent hover:bg-gray-600 text-gray-100 font-semibold text-sm"
   >
     {children}
   </Link>
--- packages/corewar-app/src/features/simulator/simulator-settings.js
+++ packages/corewar-app/src/features/simulator/simulator-settings.js
@@ -1,5 +1,42 @@
 import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { useHistory } from 'react-router-dom'
+import { getSimulatorState } from './reducer'
+import { setCoreOptions } from './actions'
+import { getCoreOptions } from '../../services/core-options'
 
-const SimulatorSettings = () => <div>I'm simulator settings</div>
+const SimulatorSettings = () => {
+  const { coreOptions } = useSelector(getSimulatorState)
+  const dispatch = useDispatch()
+  const history = useHistory()
+
+  return (
+    <div className="bg-gray-700 p-4">
+      <div className="">
+        <h3 className="text-lg font-bold mt-2 mb-6 text-gray-500" id="modal-headline">
+          Simulator settings
+        </h3>
+        <ul>
+          {coreOptions.map(opt => {
+            const options = getCoreOptions(opt.id)
+            console.log(options)
+            return (
+              <li
+                key={opt.id}
+                className="flex items-center my-4 p-4 w-full text-sm rounded-lg border border-gray-600 cursor-pointer hover:bg-gray-600"
+                onClick={() => {
+                  dispatch(setCoreOptions(opt.id))
+                  history.goBack()
+                }}
+              >
+                {opt.name}
+              </li>
+            )
+          })}
+        </ul>
+      </div>
+    </div>
+  )
+}
 
 export default SimulatorSettings
