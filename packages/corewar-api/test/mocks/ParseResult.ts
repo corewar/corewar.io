2572c7c86e56d5edd1b27124d5dcfe22bdffe28f
Doug McDonald <dougajmcdonald@gmail.com>
2025-08-30 23:14:55 +0100

    fixes again

--- packages/corewar-api/test/mocks/ParseResult.ts
+++ packages/corewar-api/test/mocks/ParseResult.ts
@@ -1,14 +1,19 @@
-import { IParseResult } from 'corewar'
+import MetaData from '@/schema/MetaData'
+import ParseResult from '@/schema/ParseResult'
 
-export const buildParseResult = (): IParseResult => ({
-    messages: [],
-    metaData: {
-        author: '',
-        name: '',
-        strategy: ''
-    },
-    success: true,
-    tokens: []
-})
+export const buildParseResult = (): ParseResult => {
+    const parseResult = new ParseResult()
+    parseResult.messages = []
+    
+    const metaData = new MetaData()
+    metaData.author = ''
+    metaData.name = ''
+    metaData.strategy = ''
+    parseResult.metaData = metaData
+    
+    parseResult.success = true
+    parseResult.tokens = []
+    return parseResult
+}
 
 export default buildParseResult
