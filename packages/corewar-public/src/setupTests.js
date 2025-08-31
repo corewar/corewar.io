55fd04c49bd209ca9830604adcd5cb934f6f1f5d
Doug McDonald <dougajmcdonald@gmail.com>
2025-08-31 09:00:12 +0100

    Get CI working again

--- packages/corewar-public/src/setupTests.js
+++ packages/corewar-public/src/setupTests.js
@@ -1,8 +1,18 @@
+// Polyfill for TextEncoder in Node.js environment - must be first!
+if (typeof global.TextEncoder === 'undefined') {
+  const { TextEncoder, TextDecoder } = require('util')
+  global.TextEncoder = TextEncoder
+  global.TextDecoder = TextDecoder
+}
+
+// Also polyfill for browser APIs that might be missing
+if (typeof global.window === 'undefined') {
+  global.window = {}
+}
+
+// Configure enzyme for React 18
 import { configure } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
-var chai = require("chai")
-var sinonChai = require("sinon-chai")
 
-chai.use(sinonChai)
-
-configure({ adapter: new Adapter() });
+// Configure enzyme
+configure({ adapter: new Adapter() })
